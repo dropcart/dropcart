307648772444ad1ade3b3339a03047692ee04a41
Martijn Oud <martijn@inktweb.nl>
2015-04-30 15:45:56 +0200

    Add new version number: v1.6.1

--- _upgrade/database-v1.6.0-to-v1.6.1.sql
+++ _upgrade/database-v1.6.0-to-v1.6.1.sql
@@ -3,3 +3,8 @@
 -- 
 DELETE FROM `dc_emails_content` WHERE `navTitle` = 'Mail a friend met kortingscode';
 DELETE FROM `dc_emails` WHERE `emailName` = 'discountCodeMail';
+
+--
+-- Set version number
+--
+UPDATE  `dc_options` SET  `optionValue` =  'v1.6.1' WHERE  `dc_options`.`optionName` = 'dropcart_version'
