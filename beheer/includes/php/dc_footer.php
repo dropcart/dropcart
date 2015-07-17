69d83d22e1f50822037a65a4f803f41e55a9e291
Dmitri Chebotarev <crashbandie@hotmail.com>
2015-07-17 17:03:28 +0200

    Created off-canvas side nav in admin

--- beheer/includes/php/dc_footer.php
+++ beheer/includes/php/dc_footer.php
@@ -1,5 +1,74 @@
 </div><!-- /col -->
+</div> <!-- /.row off-canvas -->
 </div><!-- /container -->
+<script src="<?php echo SITE_URL?>/beheer/includes/script/jquery.touchSwipe.min.js"></script>
+<script>
+    $(function(){
 
+        var swipeDistanceTrigger = 80;
+
+        $(document).swipe( {
+            //Generic swipe handler for all directions
+            swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+//                alert("You swiped " + direction );
+
+                if( direction == "right" && distance >= swipeDistanceTrigger){
+                    toggleMenu('show');
+                }
+                else if(direction == "left" && distance >= swipeDistanceTrigger){
+                    toggleMenu('hide');
+                }
+
+            }
+        });
+
+
+        /* Whenever the window width changes, calculate the new min-height */
+        $(window).resize( function(){
+            setMinHeightMainContent();
+        });
+
+        /* Sets the min height of the main-content div */
+        function setMinHeightMainContent() {
+            var minHeight = $(window).outerHeight() - $('#top-bar').outerHeight() + 'px';
+            $('.main-content').css('min-height', minHeight);
+        }
+
+        /* initiate min height */
+        setMinHeightMainContent();
+
+        $('[data-toggle="offcanvas"]').click(function(){
+            toggleMenu();
+        });
+
+        $(document).on('click', '.dark-overlay', function(){
+            toggleMenu('hide');
+        });
+
+        function toggleMenu(action){
+            var nav = $('.offcanvas');
+
+            if( typeof action === "undefined") {
+                nav.toggleClass('active');
+
+            }
+
+            else if( action == "show" ){
+                nav.addClass('active');
+            }
+            else if( action == "hide"){
+                nav.removeClass('active');
+            }
+
+            /* toggle overlay */
+            if( nav.hasClass('active')){
+                $('.dark-overlay').removeClass('hidden');
+            }
+            else{
+                $('.dark-overlay').addClass('hidden');
+            }
+        }
+    });
+</script>
 </body>
 </html>
