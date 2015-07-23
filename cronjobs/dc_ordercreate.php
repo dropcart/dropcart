2599ea198bd303688fd57259e183ed5236b95391
Dmitri Chebotarev <crashbandie@hotmail.com>
2015-07-23 10:11:18 +0200

    Fixed paths and use of underfined property orderId  in dc_ordercreate.php cronjob

--- cronjobs/dc_ordercreate.php
+++ cronjobs/dc_ordercreate.php
@@ -2,20 +2,16 @@
 /**
  * Cronjob that creates orders that don't have an external orderId yet
  */
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 
-define('SITE_PATH_CRON',dirname(dirname(__FILE__)).'/'); // for cronjobs, because $_SERVER is not available
-
-require_once(SITE_PATH_CRON.'includes/php/dc_connect.php');
-require_once(SITE_PATH_CRON.'_classes/class.database.php');
+require_once(__DIR__.'/../includes/php/dc_connect.php');
+require_once(__DIR__.'/../_classes/class.database.php');
 $objDB = new DB();
 
-require_once (SITE_PATH_CRON.'includes/php/dc_config.php');
-require_once (SITE_PATH_CRON.'includes/php/dc_functions.php');
-require_once (SITE_PATH_CRON.'includes/php/dc_mail.php');
-require_once (SITE_PATH_CRON.'_classes/class.cart.php');
-require_once (SITE_PATH_CRON.'libraries/Api_Inktweb/API.class.php');// Inktweb API
+require_once (__DIR__.'/../includes/php/dc_config.php');
+require_once (__DIR__.'/../includes/php/dc_functions.php');
+require_once (__DIR__.'/../includes/php/dc_mail.php');
+require_once (__DIR__.'/../_classes/class.cart.php');
+require_once (__DIR__.'/../libraries/Api_Inktweb/API.class.php');// Inktweb API
 
 // New Inktweb Api object
 $Api = new Inktweb\API(API_KEY, API_TEST, API_DEBUG);
@@ -28,9 +24,13 @@ $strSQL =
 $result = $objDB->sqlExecute($strSQL);
 while($objOrder = $objDB->getObject($result)) {
 
-	$strJsonUrl = SITE_URL . 'beheer/dc_order_manage.php?action=export&uuid='.$objOrder->uuid.'&id='.$objOrder->orderId;
+	$strJsonUrl = SITE_URL . '/beheer/dc_order_manage.php?action=export&uuid='.$objOrder->uuid.'&id='.$objOrder->orderId;
 	$Order = $Api->importOrder($strJsonUrl);
-	
+
+	/* Do not execute code below if there is no orderId avaiable */
+	if( !isset($Order->orderId) )
+		continue;
+
 	$intExtOrderId = intval($Order->orderId);
 	
 	$strSQL = "UPDATE ".DB_PREFIX."customers_orders SET extOrderId = ".$intExtOrderId." WHERE orderId = ".$objOrder->orderId;
