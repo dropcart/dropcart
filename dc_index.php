54b24b20af0ca8e551a8c41ce55f0ada915bebe7
MarkV28 <mark_vels@hotmail.com>
2015-10-06 22:17:47 +0200

    debug more

--- dc_index.php
+++ dc_index.php
@@ -10,10 +10,10 @@ require_once 'includes/php/dc_config.php';
 // Page specific includes
 require_once '_classes/class.cart.php';
 require_once 'includes/php/dc_functions.php';
+echo 'blah';
 
 // Start API
 require_once 'libraries/Api_Inktweb/API.class.php';
-echo 'blah';
 
 // Generate page title & meta tags
 $strPageTitle = getContent('homepage_title');
