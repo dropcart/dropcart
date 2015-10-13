b8fff2474f746fa457b3ed521e0d0587d32a9406
Mark V <mark_vels@hotmail.com>
2015-10-13 18:40:43 +0200

    Change translation files

--- locale/en/translations.php
+++ locale/en/translations.php
@@ -1,32 +1,237 @@
 <?php
 
-// Vertaling Engels
+/****************************
+*
+* Vertaling Engels
+*
+****************************/
 
-$text['BASKET'] = "Shoppingcart";
-$text['ACCOUNT'] = "My account";
-$text['ARTICLES'] = "articles";
-$text['ARTICLE'] = "article";
-$text['STEP'] = "Step";
-$text['BRAND'] = "brand";
-$text['TYPE'] = "type";
-$text['MODEL'] = "model";
-$text['SELECT'] = "Select";
-$text['SEARCH'] = "Search";
-$text['SEARCH_BAR'] = "Search on articlenumber...";
-$text['CATEGORY_PAPER'] = "Paper";
+// HEADER
+$text['ACCOUNT'] = "Mijn account";
+$text['ARTICLES'] = "artikelen";
+$text['ARTICLE'] = "artikel";
+$text['CATEGORY_PAPER'] = "Papier";
 $text['CATEGORY_CARTRIDGES'] = "Cartridges";
 $text['CATEGORY_TONERS'] = "Toners";
 $text['CATEGORY_LABELS'] = "Labels";
-$text['CATEGORY_ACCESSORIES'] = "Accessories";
-$text['CATEGORY_SHOW'] = "show all";
-$text['JAVASCRIPT_ALERT_BOLD'] = "Caution:";
+$text['CATEGORY_ACCESSORIES'] = "Toebehoren";
+$text['CATEGORY_SHOW'] = "toon alles";
+$text['TEST_ALERT'] = "Deze website staat momenteel in <code>api_test</code> = <code>true</code> modes.";
+
+// JAVASCRIPT
+$text['JAVASCRIPT_ALERT_BOLD'] = "Let op:";
 $text['JAVASCRIPT_ALERT_TEXT'] = "ons systeem heeft gedetecteert dat Javascript in uw browser niet is ingeschakeld.<br />U zult Javascript moeten aanzetten om gebruik te maken van onze site.";
 $text['JAVASCRIPT_ALERT_BUTTON'] = "Hoe zet ik javascript aan?";
-$text['TEST_ALERT'] = "Deze website staat momenteel in <code>api_test</code> = <code>true</code> modes.";
-$text['SHOW_CARTRIDGES'] = "Show my cartridges";
-$text['BACK_TO_TOP'] = "Back to top";
-$text['ABOUT_BOLD'] = "Cheap printing with";
+$text['PAGE_TITLE_JAVASCRIPT'] = "Javascript aanzetten";
+$text['PAGE_DESCRIPTION_JAVASCRIPT'] = "Hier vind je een korte uitleg over hoe javascript kan aanzetten in de browser.";
+$text['PAGE_CONTENT_JAVASCRIPT']['TITLE'] = "Javascript activeren";
+$text['PAGE_CONTENT_JAVASCRIPT']['SUBTITLE'] = "Waarom zou je Javascript activeren?";
+$text['PAGE_CONTENT_JAVASCRIPT']['CONTENT'] = "Als je JavaScript activeert, kun je optimaal genieten van je bezoek aan onze webshop, zonder lastig gevallen te worden door irritante foutmeldingen. De meeste moderne webbrowsers bieden een ingebouwde ondersteuning voor JavaScript.<br />Kies uit onderstaande lijst je webbrowser en volg de bijbehorende stappen om in een handomdraai JavaScript te activeren. Zorg ervoor dat je de meest recente versie van je browser geïnstalleerd hebt.";
+
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['ID'] = "firefox";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['NAME'] = "Mozilla Firefox";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['LIST']['FIRST'] = "Klik op het (oranje) menu <strong>'Firefox'</strong> (Windows) of het menu <strong>'Firefox'</strong> (Mac)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['LIST']['SECOND'] = "Kies <strong>'Opties'</strong> (Windows) of <strong>'Voorkeuren'</strong> (Mac)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['LIST']['THIRD'] = "Kies <strong>'Inhoud'</strong> in het bovenste navigatievak";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['LIST']['FOURTH'] = "Vink het selectievakje bij <strong>'JavaScript inschakelen'</strong> aan";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['FIREFOX']['LIST']['LAST'] = "Klik op <strong>'OK'</strong>";
+
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['ID'] = "ie";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['NAME'] = "Microsoft Internet Explorer";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['FIRST'] = "Klik op het tandwiel rechts boven aan (of het menu <strong>'Extra'</strong>)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['SECOND'] = "Kies <strong>'Internetopties'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['THIRD'] = "Klik op het tabblad <strong>'Beveiliging'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['FOURTH'] = "Klik op <strong>'Aangepast niveau'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['FIFTH'] = "Scroll naar het gedeelte over <strong>'Uitvoeren van scripts'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['SIXTH'] = "Selecteer <strong>'Inschakelen'</strong> bij <strong>'Actief uitvoeren van scripts'</strong> en <strong>'Java-applets uitvoeren in scripts'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['EXPLORER']['LIST']['LAST'] = "Klik op <strong>'OK'</strong>";
+
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['ID'] = "opera";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['NAME'] = "Opera";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['FIRST'] = "Klik op het (rode) menu <strong>'Opera'</strong> (Windows) of op het menu <strong>'Opera'</strong> (Mac)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['SECOND'] = "Kies <strong>'Instellingen'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['THIRD'] = "Kies<strong> 'Voorkeuren'</strong> in het bovenste navigatievak";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['FOURTH'] = "Kies <strong>'Inhoud'</strong> in de navigatielijst links";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['FIFTH'] = "Kies <strong>'Inhoud'</strong> in de navigatielijst links";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['OPERA']['LIST']['LAST'] = "Klik op <strong>'OK'</strong>";
+
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['ID'] = "chrome";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['NAME'] = "Google Chrome";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['LIST']['FIRST'] = "Klik bij de moersleutel rechts boven aan";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['LIST']['SECOND'] = "Klik op <strong>'Opties'</strong> (Windows) of selecteer <strong>'Voorkeuren'</strong> in het Chrome menu (Mac)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['LIST']['THIRD'] = "Klik op de tab <strong>'Geavanceerde opties'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['LIST']['FOURTH'] = "Klik op <strong>'Instellingen voor inhoud...'</strong> onder <strong>'Privacy'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['CHROME']['LIST']['LAST'] = "Kies <strong>'Alle sites toestaan om Javascript uit te voeren (aanbevolen)'</strong> onder <strong>'JavaScript'</strong>";
+
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['ID'] = "safari";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['NAME'] = "Apple Safari";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['FIRST'] = "Klik op het menu <strong>'Safari'</strong> (Mac) of het tandwiel rechts boven aan (of het menu <strong>'Wijzigen'</strong>) (Windows)";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['SECOND'] = "Kies <strong>'Voorkeuren'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['THIRD'] = "Kies <strong>'Beveiliging'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['FOURTH'] = "Vink het selectievakje naast <strong>'JavaScript activeren'</strong> aan";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['FIFTH'] = "Klik op <strong>'JavaScript inschakelen'</strong>";
+$text['PAGE_CONTENT_JAVASCRIPT']['BROWSER']['SAFARI']['LIST']['LAST'] = "Klik op <strong>'OK'</strong>";
+
+// SELECTOR
+$text['STEP'] = "Stap";
+$text['BRAND'] = "merk";
+$text['TYPE'] = "type";
+$text['MODEL'] = "model";
+$text['SELECT'] = "selecteer";
+$text['SHOW_CARTRIDGES'] = "Toon mijn cartridges";
+
+// FOOTER
+$text['BACK_TO_TOP'] = "Omhoog";
+$text['ABOUT_BOLD'] = "Goedkoop printen met";
 $text['ABOUT_TEXT_1'] = "Wij leveren printer inkt cartridges voor alle grote merken zoals HP, Brother, Epson en Canon. Gebruik onze <em>cartridge zoeker</em> hierboven om de juiste producten voor uw printer te vinden en browse door ons uitgebreid assortiment laag geprijsde en hoogwaardige printer verbruiksartikelen.";
 $text['ABOUT_TEXT_2'] = "Producten die op voorraad zijn worden nog dezelfde dag verzonden!";
+
+// 404
+$text['PAGE_TITLE_404'] = "Deze pagina bestaat niet";
+$text['PAGE_DESCRIPTION_404'] = "De pagina die u heeft opgevraagd bestaat niet!";
+$text['PAGE_CONTENT_404_1'] = "De pagina die u heeft opgevraagd kon niet worden gevonden door ons systeem.<br />Neem gerust contact met ons op via";
+$text['PAGE_CONTENT_404_2'] = "als u er niet uitkomt.";
+
+// CATEGORY
+$text['SUITABLE'] = "Geschikt voor";
+
+// SORT
+$text['SORT'] = "Sorteer op";
+$text['SORT_SALES_DESC'] = "Populair";
+$text['SORT_PRICE_ASC'] = "Laagste prijs";
+$text['SORT_PRICE_DESC'] = "Hoogste prijs";
+$text['SORT_TITLE_ASC'] = "Titel A-Z";
+
+// NO RESULT
+$text['NO_RESULTS_1'] = "Geen resultaten gevonden.";
+$text['NO_RESULTS_2'] = "Geen geschikte inkt cartridges, toners papier of andere toebehoren gevonden voor deze printer. Misschien dat u met onderstaande links verder komt?";
+
+// LOGIN
+$text['WRONG_PASSWORD'] = "Wachtwoord is niet juist.";
+$text['UNKOWN_MAIL'] = "Email is niet bekend";
+$text['ERROR'] = "Fout!";
+$text['LOGIN_SUBTITLE'] = "Heeft u al een account?";
+$text['INPUT_EMAIL'] = "Email";
+$text['INPUT_PASSWORD'] = "Wachtwoord";
+$text['LOGIN_BUTTON'] = "Inloggen";
+$text['FORGOT_PASSWORD'] = "Wachtwoord vergeten?";
+
+// PASS_RESET
+$text['LINK_EXPIRED'] = "De link is niet geldig.";
+$text['NO_ACCOUNT'] = "Onder dit emailadres is geen account aanwezig.";
+$text['LINK_SEND'] = "Een email met een link om het wachtwoord te resetten is onderweg.";
+$text['SUCCESS'] = "Gelukt!";
+$text['PASS_RESET_CONTENT'] = "Vul hieronder uw emailadres in en wij zullen u een link toesturen om uw wachtwoord te resetten.";
+$text['PASS_RESET_BUTTON'] = "Wachtwoord resetten";
+$text['PASS_RESET_BACK'] = "Wacht, ik weet het weer!";
+
+// PRINTER PRODUCT
+$text['PRINTER_TITLE'] = "Producten geschikt voor de";
+
+// PRODUCT DETAILS
+$text['GARANTY'] = "1 jaar garantie";
+$text['TIME_TO_CONSIDER'] = "14 dagen bedenktermijn";
+$text['SELLING_POINT'] = "Uitstekende prijs- kwaliteitsverhouding";
+$text['TAX'] = "inclusief BTW";
+$text['NO_TAX'] = "exclusief BTW";
+$text['SAVINGS_1'] = "Koop";
+$text['SAVINGS_2'] = "stuks voor";
+$text['SAVINGS_3'] = "per stuk en <strong>bespaar";
+$text['STOCK'] = "Voorraad";
+$text['STOCK_SURPLACE'] = "Ruimschoots op voorraad";
+$text['IN_STOCK'] = "Op voorraad";
+$text['OUT_STOCK'] = "Niet op voorraad (circa 3 werkdagen levertijd)";
+$text['DELIVER_TIME_IN_STOCK_PREFIX'] = "<strong>Levertijd</strong>: Besteld voor 20:00,";
+$text['DELIVER_TIME_IN_STOCK_AFFIX'] = "morgen in huis";
+$text['DELIVER_TIME_OUT_STOCK'] = "<strong>Levertijd</strong>: Circa 3 werkdagen</p>";
+$text['QUANTITY'] = "Aantal";
+$text['ADD_TO_CART'] = "Toevoegen aan winkelmand";
+$text['NO_SUPPLY'] = "(Niet leverbaar)";
+$text['DESCRIPTION'] = "Omschrijving";
+$text['SUITABLE_PRINTERS_TITLE'] = "Geschikte printers";
+$text['SUITABLE_PRINTERS_CONTENT'] = "Dit product is gegarandeerd geschikt voor de volgende printers";
+$text['ADDED_TO_CART_TITLE'] = "Toegevoegd aan winkelwagen";
+$text['ADDED_TO_CART_CONTENT'] = "Wij hebben dit product aan uw winkelwagen toegevoegd.";
+$text['CLOSE'] = "Sluiten";
+$text['SHOW_CART'] = "Bekijk winkelwagen";
+$text['TOP_5'] = "Top 5 Meepakkers";
+
+// PROFILE
+$text['LOGOUT'] = "(uitloggen?)";
+$text['PERSONAL_DATA'] = "Persoonlijke gegevens";
+$text['COMPANY'] = "Bedrijfsnaam";
+$text['NAME'] = "Naam";
+$text['GENDER'] = "Geslacht";
+$text['UNKOWN'] = "Onbekend";
+$text['MALE'] = "Mannelijk";
+$text['FEMALE'] = "Vrouwelijk";
+$text['EMAIL'] = "Emailadres";
+$text['CHANGE'] = "Wijzigen";
+$text['MY_STREETS'] = "Mijn adressen";
+$text['STREET'] = "Adres";
+$text['STREETS'] = "Adressen";
+$text['ZIP_CODE'] = "Postcode";
+$text['CITY'] = "Woonplaats";
+$text['ORDERS'] = "Bestellingen";
+$text['ORDER_ID'] = "Ordernummer";
+$text['ORDER_DATE'] = "Besteldatum";
+$text['ORDER_ITEMS'] = "Items";
+$text['ORDER_PAYMENT'] = "Betaalmethode";
+$text['ORDER_STATE'] = "Status";
+$text['NO_ORDERS'] = "Wij hebben (nog?) geen bestellingen van u gevonden.";
+$text['DETAILS'] = "Klik om order details te bekijken";
+
+// PROFILE ADRESSES
+$text['ADRESS_CHANGED'] = "Adres is aangepast.";
+$text['SOMETHING_WENT_WRONG'] = "Er is iets fout gegaan.";
+$text['SELECT_STREET'] = "Selecteer een adres om deze aan te passen";
+$text['FIRST_NAME'] = "Voornaam";
+$text['LAST_NAME'] = "Achternaam";
+$text['STREET_NAME'] = "Straatnaam";
+$text['STREET_NUMBER'] = "Huisnummer";
+$text['STREET_NUMBER_AFFIX'] = "Huisnummer toevoegingen";
+$text['COUNTRY'] = "Land";
+$text['DEFAULT_BILLING_ADRESS'] = "Dit adres is mijn standaard factuuradres";
+$text['DEFAULT_DELIVERY_ADRESS'] = "Dit adres is mijn standaard afleveradres";
+$text['ADJUST'] = "Aanpassen";
+$text['MODIFY_ADRESS'] = "Adres bewerken";
+
+// PROFILE EDIT
+$text['DIFFERENT_PASSWORDS'] = "Wachtwoorden komen niet overeen.";
+$text['USER_CHANGED'] = "Gebruiker is aangepast";
+$text['CUSTOMER_SINCE'] = "Klant sinds";
+$text['EMPTY_PASSWORD_FIELDS'] = "Laat de velden leeg als u het wachtwoord niet wenst te resetten";
+$text['REPEAT_PASSWORD'] = "Wachtwoord herhalen...";
+
+// SEARCH
+$text['SEARCH'] = "Zoeken";
+$text['SEARCH_BAR'] = "Zoek op artikelnummer...";
+$text['SEARCH_RESULTS'] = "Resultaten voor";
+$text['NO_SEARCH_RESULTS'] = "Geen resultaten gevonden. Probeer uw zoekterm te verfijnen";
+
+// SHOPPINGCART
+$text['SHOPPING_BASKET'] = "Winkelmand";
+$text['SHOPPING_CART'] = "Winkelwagen";
+$text['SHOPPING_DATA'] = "Gegevens";
+$text['SHOPPING_PAYMENT'] = "Betaling";
+$text['ORDER_PLACED'] = "Bestelling geplaatst";
+$text['PRODUCT'] = "Product";
+$text['PRICE'] = "Prijs";
+$text['TOTAL'] = "Totaal";
+$text['STOCK_LOW'] = "Niet voldoende op voorraad";
+$text['DELETE_FROM_BASKET'] = "Verwijder artikel uit winkelmandje";
+$text['DISCOUNT_CODE'] = "Kortingcode";
+$text['SUBTOTAL'] = "Subtotaal";
+$text['SHIPPING_FEE'] = "Verzendkosten";
+$text['VOUCHER_CONTENT'] = "Heeft u een korting of vouchercode?";
+$text['ADD_VOUCHER_CODE'] = "Code invoeren";
+$text['YOUR_VOUCHER_CODE'] = "Uw kortingcode...";
+$text['SEND'] = "Versturen";
+$text['SEND_ORDER'] = "Bestelling afronden";
+$text['NO_ARTICLES_IN_BASKET'] = "Geen producten in winkelwagen.";
+$text['IN_SHOPPING_CART'] = "In winkelwagen";
+$text['VERIFICATION_CODE_CONTENT'] = "Voor deze code is een controlecode vereist.<br/>Vul uw controlecode in die u heeft ontvangen.";
+$text['YOUR_VERIFICATION_CODE'] = "Uw controlecode..";
+$text['REMOVE'] = "Verwijder";
 
 ?>
