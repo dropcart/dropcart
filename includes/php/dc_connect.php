3c26ad6fe59655d018d681ce42faf49f1b7d2e80
erikstelt <erikstelt@gmail.com>
2017-07-13 17:03:12 +0200

    Removed double session starting. This seemes to give some problems with PHP 7

--- includes/php/dc_connect.php
+++ includes/php/dc_connect.php
@@ -4,7 +4,7 @@
  */
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USER', '');
+define('DB_NAME', 'dropcartv1');
+define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_PREFIX', 'dc_'); // changing this currently not (fully) supported
